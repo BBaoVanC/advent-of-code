450469d4892151e63999b897607beb02c2ef555c
BBaoVanC <github@bbaovanc.com>
2020-12-02 23:48:32 -0600

    Finish day2.py

--- 2020/day2/day2.py
+++ 2020/day2/day2.py
@@ -4,13 +4,30 @@ import sys
 
 rawInput = sys.stdin.readlines()
 
-check = rawInput[0]
-split1tuple = check.partition(":")  # ("1-3 a", ":", "abcde")
-split1 = list(split1tuple)  # convert to list
-split1[-1] = split1[-1].strip();  # remove newline from last item
+valid = 0
+invalid = 0
+for check in rawInput:
+    split1tuple = check.partition(":")  # ("1-3 a", ":", "abcde")
+    split1 = list(split1tuple)  # convert to list
+    split1[-1] = split1[-1].strip()  # remove newline from last item
 
-split2tuple = split1[0].partition("-")  # ("1", "-", "3 a")
-split2 = [split2tuple[0], split2tuple[2][0], split2tuple[2][-1]]  # ["1", "3", "a"]
+    split2tuple = split1[0].partition("-")  # ("1", "-", "3 a")
+    split2str = [split2tuple[0], split2tuple[2][0], split2tuple[2][-1]]  # ["1", "3", "a"]
+    split2 = [int(split2str[0]), int(split2str[1]), split2str[2]]  # [1, 3, "a"]
 
-if true:
-    print("It works!")
+    ccount = 0
+    for char in split1[2]:
+        if char == split2[2]:
+            ccount += 1
+
+    if split2[0] <= ccount <= split2[1]:
+        valid += 1
+        print(f"Valid:      {check.strip()}")
+    else:
+        invalid += 1
+        print(f"Invalid:    {check.strip()}")
+
+print()
+print("---> SUMMARY")
+print(f"Valid:      {valid}")
+print(f"Invalid:    {invalid}")
